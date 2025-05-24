e25d47082edc6123a48cc7701e6e009328be8a9d
Lynxman21 <mateuszrys97@gmail.com>
2025-05-24 17:30:48 +0200

    NewClassesVW

--- Maven/GameLib/src/main/java/Entities/GameCountVW.java
+++ Maven/GameLib/src/main/java/Entities/GameCountVW.java
@@ -1,4 +1,44 @@
 package Entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "game_count_vw")
 public class GameCountVW {
+    @Id
+    @OneToOne
+    @JoinColumn(name = "game_id")
+    @Column(name = "game_id")
+    private Game game;
+
+    private String name;
+
+    private Long amount;
+
+    public GameCountVW() {
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
 }
--- Maven/GameLib/src/main/java/dao/GameDAO.java
+++ Maven/GameLib/src/main/java/dao/GameDAO.java
@@ -1,6 +1,7 @@
 package dao;
 
 import Entities.Game;
+import Entities.GameCountVW;
 import org.hibernate.HibernateError;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -17,13 +18,13 @@ public class GameDAO {
     }
 
     public List<Game> FindAllAvailableGames() {
-//        try (Session session = sessionFactory.openSession()) {
-//            return session.createQuery(
-//                    "from Game where "
-//            )
-//        } catch (Exception e) {
-//            System.out.println("Błąd podczas wyszukiwania gier: " + e.getMessage());
-//            return Collections.emptyList();
-//        }
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery(
+                    "select c.game from GameCountVW c where c.amount > 0", Game.class
+            ).list();
+        } catch (Exception e) {
+            System.out.println("Błąd podczas wyszukiwania gier: " + e.getMessage());
+            return Collections.emptyList();
+        }
     }
 }
